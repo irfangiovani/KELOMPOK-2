5f8f347cef21ad043f2e7320d338fbaf5a5a35ba
MUHAMMAD ANSORI <ansoriunyu33@gmail.com>
2019-10-02 12:26:23 +0700

    html.php 1

--- ansori/html.php
+++ ansori/html.php
@@ -1,18 +1,29 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
 <head>
-	<title>
-		tugas 1
-	</title>
+  <title>keos</title>
 </head>
-<body>
-	<center>
-	<h1 style = "color :green">selamat belajar html!</h1>
-	<h2 style ="color :aqua">Nama Saya : Muhammad Ansori</h2>
-	<h3>Politeknik Negri Jember</h3></center> 
-	<form action="" metod="post" style="background-color: #if">
-		<ul>
-			<li><center>label 
 
+<body bgcolor="cyan"></body>
+<h1><center>oyi_cak</center></h1>
+<hr>
+<marquee behavior="alternate" direction="left">bik-kadibik</marquee>
+
+  <h2 style="color: yellow"> belajar html</h2>
+  <h2 style="color: yellowgreen">Nama saya : Muhammad Ansori</h2>
+  <h2>politeknik negeri jember kampus bondowoso</h2>
+  <h2>cek lessonah sarah</h2>
+</hr>
+ <?php 
+echo '<h1><center>oyi_cak</center></h1>
+<hr>
+<marquee behavior="alternate" direction="left">dewe-dewe</marquee>
+
+  <h2 style="color: yellow"> belajar html</h2>
+  <h2 style="color: yellowgreen">Nama saya : Muhammad Ansori</h2>
+  <h2>politeknik negeri jember kampus bodowoso</h2>
+  <h2>cek lessonah sarah</h2>
+</hr>';
+  ?>
 </body>
 </html>
