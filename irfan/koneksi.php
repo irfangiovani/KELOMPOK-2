693af03474ca68501b7aec6b9ebe254159d8fe5c
irfan giovani <irfangiovani987@gmail.com>
2019-10-09 18:28:29 +0700

    Update koneksi.php

--- irfan/koneksi.php
+++ irfan/koneksi.php
@@ -20,8 +20,24 @@ echo"koneksi berhasil";
     <input type="text" name="x" placeholder="Username" class="form">
     <br>
     password:<br>
-    <input type="text" name="y" placeholder="password" class="form">
+    <input type="text" name="y" placeholder="password">
     <br><br>
     <input type="submit" name="lo" value="Masuk" class="btn btn-primary">
   
 </form>
+
+<?php
+if(isset($_POST['lo'])){
+    $username = filter_input(IMPUT_POST,'X', FILTER_SANITIZE_STRING);
+    $password = filter_input(IMPUT_POST,'y', FILTER_SANITIZE_STRING);
+ if ($username=="admin"&& $password=="root"){
+     session_start();
+     $_SESSION["user"] = $username;
+     // header("Location: per8.php");
+     echo "</br></br>Berhasil Login";
+ } else{
+     echo"</br></br>Gagal Login";
+ 
+ }
+}
+?>
