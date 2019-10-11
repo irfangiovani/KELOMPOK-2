083c095b36683da592783b713f5d3d198eb98833
azizah wina <azizahwinasriwinarsih24@gmail.com>
2019-10-11 19:22:47 +0700

    Tugas BKPM Praktek 4

---
+++ Wina/E41182254_P4/kondisi_if_else.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script src="jquery-3.3.1.slim.min.js"></script>
+	<script src="popper.min.js"></script>
+	<script src="ajax.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="style.css">
+	<title>Halaman Login Admin</title>
+</head>
+<body>
+
+<form method="get" action="tampil.php">
+	<label>Masukkan Nama</label><br/>
+	<input type="text" name="nama"><br/>
+
+</body>
+</html>
---
+++ Wina/While.php
@@ -0,0 +1,12 @@
+<?
+///perulang while pada php
+    while( Kondisi ){
+    Statement.
+    }
+    $x = 1; 
+ 
+    while($x <= 10) {
+        echo "Angka $x <br>";
+        $x++;
+    }
+?>
