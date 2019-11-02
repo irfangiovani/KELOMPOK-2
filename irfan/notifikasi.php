923a1d1c862cf5f08bdb6bd14174014f8bfbba73
irfan giovani <irfangiovani987@gmail.com>
2019-11-02 19:00:45 +0700

    sabtu

---
+++ irfan/notifikasi.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>penembalian buku</title>
+</head>
+    <h2>notifikasi</h2>
+    
+    
+    <table border= "1" cellpadding="10">
+        <tr>
+            <td>No</td>
+            <td>nama</td>
+            <td>alamat</td>
+            <td>Nim</td>
+            <td>jurusan</td>
+            <td>opsi</td>
+        
+         </tr>
+         <?php
+         //include 'koneksi.php';//
+         $no = 1;
+         $select = mysqli_query($koneksi, "SELECT * FROM identitas");
+         while($hasil = mysqli_fetch_array($select)){
+         ?>
+<tr>
+            <td><?php echo $no++?></td>
+            <td><?php echo$hasil['nama']?></td>
+            <td><?php echo$hasil['alamat']?></td>
+            <td><?php echo$hasil['nim']?></td>
+            <td><?php echo$hasil['jurusan']?></td>
+            <td>
+                <a href="">Edit</a>
+                <a href="">Hapus</a>
+                
+            </td>
+            <a href="tambah.php">tambah</a>
+<?php }?>
+
+            
+</tr>
+</table>
+</body>
+</html>
