9e1883bcb1f1be989ea007ad02be7d092851b7c1
riyan-web <yudiiriyanto7@gmail.com>
2019-11-25 07:59:56 +0700

    aktif member

--- perpustakaan/admin/aktif_member.php
+++ perpustakaan/admin/aktif_member.php
@@ -8,121 +8,119 @@ if( !isset($_SESSION["login"])){
   $id = $_GET["id"];
   $data = query("SELECT * FROM member_perpus WHERE nis = '$id'")[0];
 
-  if( isset($_POST["submit"]) ) {
-
-  if( ubah($_POST) > 0 ) {
-    echo "
-        <script>
-            alert('member berhasil disetujui!');
-            document.location.href = 'data_member.php';
+  if( isset($_POST["submit"])){
+    $nis = $_POST["nis"];
+    $nama = $_POST["nama"];
+    $kelas = $_POST["kelas"];
+    $jurusan = $_POST["jurusan"];
+    $no_telp = $_POST["no_telp"];
+    $alamat = $_POST["alamat"];
+    $status ="aktif";
+    //query insert data
+    $tambah = mysqli_query($conn, "UPDATE member_perpus SET nis='$nis', nama_siswa='$nama',kelas='$kelas',jurusan='$jurusan', no_telp='$no_telp',  alamat='$alamat','$status'");
+    var_dump($tambah);
+    if($tambah){
+        ?>
+        <script type="text/javascript">
+          alert('member berhasil disetujui!');
+          document.location.href="data_member.php";
         </script>
-    ";
-  } else {
-    echo "
-        <script>
-            alert('member belum disetujui!');
-            document.location.href = 'data_member.php';
-        </script>
-    ";
-  }
+        <?php
+      }else {
+        echo "gagal mengedit data!!!";
+      }
+    }
 
-}
+
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- bootstrap CSS -->
-    <link rel="stylesheet" href="css/css/bootstrap.min.css">
-    <title>Ubah Buku Literasi Umum</title>
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
+
+    <!-- Title Page-->
+    <title>Au Register Forms by Colorlib</title>
+
+    <!-- Icons font CSS-->
+    <link href="../vendor/register_member/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="../vendor/register_member/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="../vendor/register_member/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="../vendor/register_member/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="../css/main.css" rel="stylesheet" media="all">
 </head>
+
 <body>
-    <div class="container">
-    <h2 class="alert alert-success text-center mt-3">Ubah Buku Literasi Umum</h1>
-    
-    <form action="" method="post" enctype="multipart/form-data">
-    
-           <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="kode_buku_literasi">Kode Buku Literasi : </label>
-                  <input type="hidden" class="form-control"  name="kode_buku_literasi" value ="<?php echo $data['kode_buku_literasi']; ?>" id="kode_buku_literasi">
-                  <input type="hidden" class="form-control"  name="gambarLama" value ="<?php echo $data['gambar_sampul']; ?>" id="kode_buku_literasi">
-            </div>
-                <div class="form-group col-md-6">
-                  <label for="kategori">Kategori : </label> <a href="kategori.php" class="btn btn-warning" title="tambah_kategori" >Tambah Kategori</a>
-                  <input type="text" class="form-control" value ="<?php echo $data['id_kategori'] ?>" name="id_kategori" id="id_kategori">
-                 
+    <div class="page-wrapper bg-blue p-t-100 p-b-100 font-robo">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-1">
+                <div class="card-heading"></div>
+                <div class="card-body">
+                    <h2 class="title">Registration Info</h2>
+                    <form action = "" method="POST">
+                        <div class="input-group">
+                            <input class="input--style-1" type="text" placeholder="NIS" name="nis" id="nis"
+                            value ="<?php echo $data['nis']; ?>">
+                        </div>
+                    <form method="POST">
+                        <div class="input-group">
+                             <input class="input--style-1" type="text" placeholder="NAMA" name="nama" id="nama"
+                             value ="<?php echo $data['nama_siswa']; ?>">
+                        </div>
+                    <form method="POST">
+                        <div class="input-group">
+                            <input class="input--style-1" type="text" placeholder="KELAS" name="kelas" id="kelas"
+                            value ="<?php echo $data['kelas']; ?>">
+                        </div>
+                    <form method="POST">
+                        <div class="input-group">
+                            <input class="input--style-1" type="text" placeholder="JURUSAN" name="jurusan" id="jurusan"
+                            value ="<?php echo $data['jurusan']; ?>">
+                        </div>
+                    <form method="POST">
+                        <div class="input-group">
+                            <input class="input--style-1" type="text" placeholder="NO TELP" name="no_telp"id="no_telp"
+                            value ="<?php echo $data['no_telp']; ?>">
+                         </div>
+                    <form method="POST">
+                        <div class="input-group">
+                            <input class="input--style-1" type="text" placeholder="ALAMAT" name="alamat" id="alamat"
+                            value ="<?php echo $data['alamat']; ?>">
+                        </div>
+                        </div>
+                        </div>
+                        <div class="p-t-20">
+                            <button class="btn btn--radius btn--green" type="submit" name="submit">SIMPAN</button>
+                        </div>
+                    </form>
                 </div>
-           </div>
-
-            <div class="form-group text-center">
-                  <label for="judul_buku_literasi">Judul Buku Literasi : </label>
-                  <input type="text" class="form-control" value ="<?php echo $data['judul_buku_literasi'] ?>" name="judul_buku_literasi" id="judul_buku_literasi">
             </div>
-
-            <div class="form-row">
-                <div class="form-group col-md-4">
-                  <label for="penerbit">Penerbit : </label>
-                  <input type="text" class="form-control"value ="<?php echo $data['id_penerbit'] ?>" name="id_penerbit" id="id_penerbit">
-                </div>
-
-                <div class="form-group col-md-4">
-                  <label for="tahun_terbit">Tahun Terbit : </label>
-                  <input type="text" class="form-control" value ="<?php echo $data['tahun_terbit'] ?>" name="tahun_terbit" id="tahun_terbit">
-                </div>
-
-                <div class="form-group col-md-4">
-                  <label for="no_rak">No Rak : </label>
-                  <input type="text" class="form-control" value ="<?php echo $data['id_rak'] ?>" name="id_rak" id="id_rak">
-                </div>
-            </div>
-
-            <div class="form-group text-center">
-              <label for="deskripsi_buku">Deskripsi Buku : </label>
-              <textarea type="text" value ="<?php echo $data['deskripsi_buku'] ?>" name="deskripsi_buku" id="deskripsi_buku" class="form-control" rows="5" > <?php echo $data['deskripsi_buku'] ?></textarea>
-            </div>
-
-            <div class="form-group">
-              <label for="gambar_sampul">Gambar Sampul : <?php echo $data['gambar_sampul'] ?></label><br>
-              <img src="img/literasi/<?= $data['gambar_sampul']; ?>" width="60\">
-              <input type="file" class="form-control-file" id="gambar_sampul">
-              <small>(Upload File Dengan Ukuran Maksiman 1 MB)</small>
-            </div>
-
-            <div class="text-center">
-              <button type="submit" class="btn btn-primary" name="edit">Edit Data!</button>
-              <button type="reset" class="btn btn-danger">RESET</button>
-            </div>
-        <br><br>
+        </div>
     </div>
-    <?php
-    if( isset($_POST["edit"])){
-    // ambil data dari tiap elemen dalam form
-    $kode_buku_literasi = $_POST["kode_buku_literasi"];
-    $judul_buku_literasi = $_POST["judul_buku_literasi"];
-    $id_penerbit = $_POST["id_penerbit"];
-    $tahun_terbit = $_POST["tahun_terbit"];
-    $id_rak = $_POST["id_rak"];
-    $id_kategori = $_POST["id_kategori"];
-    $gambar_sampul = $_POST["gambar_sampul"];
-    $deskripsi_buku = $_POST["deskripsi_buku"];
-    $tambah = mysqli_query($conn, "UPDATE buku_literasi_umum SET kode_buku_literasi='$kode_buku_literasi', judul_buku_literasi='$judul_buku_literasi', 
-    id_penerbit='$id_penerbit', tahun_terbit='$tahun_terbit', id_rak='$id_rak', id_kategori='$id_kategori', gambar_sampul='$gambar_sampul', deskripsi_buku='$deskripsi_buku' WHERE kode_buku_literasi = '$id'");
 
-    if($tambah){
-      ?>
-      <script type="text/javascript">
-        alert('Edit Data Berhasil');
-        document.location.href="literasi.php";
-      </script>
-      <?php
-    }else {
-      echo "gagal mengedit data!!!";
-    }
+    <!-- Jquery JS-->
+    <script src="../vendor/register_member/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="../vendor/register_member/select2/select2.min.js"></script>
+    <script src="../vendor/register_member/datepicker/moment.min.js"></script>
+    <script src="../vendor/register_member/datepicker/daterangepicker.js"></script>
 
-    }
-    ?>
-       
+    <!-- Main JS-->
+    <script src="../js/register_member/global.js"></script>
+
 </body>
+
 </html>
+
+       
