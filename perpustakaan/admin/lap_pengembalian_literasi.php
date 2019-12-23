071894f390d01bbdf50aba6a109a375cbb52f45b
riyan-web <yudiiriyanto7@gmail.com>
2019-12-23 13:41:00 +0700

    u-pdate

--- perpustakaan/admin/lap_pengembalian_literasi.php
+++ perpustakaan/admin/lap_pengembalian_literasi.php
@@ -2,21 +2,23 @@
 include 'functions.php';
 require('assets/pdf/fpdf.php');
 
-if (isset($_POST['tampilkan'])) {
-	$cek = $_POST['cek'];
-	$tgl_awal = $_POST['tgl_awal'];
-	$tgl_akhir = $_POST['tgl_akhir'];
+$peminjaman_literasi = query ("SELECT * FROM peminjaman_buku_literasi a RIGHT JOIN pengembalian_buku_literasi b ON a.id_pinjam_buku_literasi = b.id_pinjam_buku_literasi "); 
 
-	if ($cek == 1) {
-	$query1= "SELECT  tb_pelanggan.nama_pelanggan, trans_jual.kd_jual, trans_jual.tgl, detail_jual.jumlah, detail_jual.harga_normal, detail_jual.diskon, detail_jual.kd_jual, detail_jual.total_harga, detail_jual.kd_barang FROM trans_jual  LEFT JOIN detail_jual on detail_jual.kd_jual = trans_jual.kd_jual  LEFT JOIN tb_pelanggan on tb_pelanggan.kd_pelanggan = trans_jual.kd_pelanggan";
-		$total = "SELECT sum(total_harga) as total_harga, sum(jumlah) as jumlah, sum(diskon) as diskon  FROM detail_jual";
-		// $query2 = "SELECT tb_pelanggan.nama_toko, trans_jual.kd_jual, trans_jual.tgl_jual FROM trans_jual LEFT JOIN tb_pelanggan on tb_pelanggan.kd_pelanggan = trans_jual.kd_pelanggan WHERE kd_jual ='$kd_jual'";
-	}
-	if($cek==2){
-		$query1= "SELECT  tb_pelanggan.nama_pelanggan, trans_jual.kd_jual, trans_jual.tgl, detail_jual.jumlah, detail_jual.harga_normal, detail_jual.diskon, detail_jual.kd_jual, detail_jual.total_harga, detail_jual.kd_barang FROM trans_jual  LEFT JOIN detail_jual on detail_jual.kd_jual = trans_jual.kd_jual LEFT JOIN tb_pelanggan on tb_pelanggan.kd_pelanggan = trans_jual.kd_pelanggan WHERE tgl>='$tgl_awal' AND tgl <='$tgl_akhir'";
-		$total = "SELECT sum(total_harga) as total_harga, sum(jumlah) as jumlah, sum(diskon) as diskon FROM detail_jual INNER JOIN trans_jual ON trans_jual.kd_jual = detail_jual.kd_jual WHERE trans_jual.tgl>='$tgl_awal' AND trans_jual.tgl <='$tgl_akhir' ORDER BY tgl asc";
-	}
-}
+// if (isset($_POST['tampilkan'])) {
+// 	$cek = $_POST['cek'];
+// 	$tgl_awal = $_POST['tgl_awal'];
+// 	$tgl_akhir = $_POST['tgl_akhir'];
+
+// 	if ($cek == 1) {
+// 	$query1= "SELECT  tb_pelanggan.nama_pelanggan, trans_jual.kd_jual, trans_jual.tgl, detail_jual.jumlah, detail_jual.harga_normal, detail_jual.diskon, detail_jual.kd_jual, detail_jual.total_harga, detail_jual.kd_barang FROM trans_jual  LEFT JOIN detail_jual on detail_jual.kd_jual = trans_jual.kd_jual  LEFT JOIN tb_pelanggan on tb_pelanggan.kd_pelanggan = trans_jual.kd_pelanggan";
+// 		$total = "SELECT sum(total_harga) as total_harga, sum(jumlah) as jumlah, sum(diskon) as diskon  FROM detail_jual";
+// 		// $query2 = "SELECT tb_pelanggan.nama_toko, trans_jual.kd_jual, trans_jual.tgl_jual FROM trans_jual LEFT JOIN tb_pelanggan on tb_pelanggan.kd_pelanggan = trans_jual.kd_pelanggan WHERE kd_jual ='$kd_jual'";
+// 	}
+// 	if($cek==2){
+// 		$query1= "SELECT  tb_pelanggan.nama_pelanggan, trans_jual.kd_jual, trans_jual.tgl, detail_jual.jumlah, detail_jual.harga_normal, detail_jual.diskon, detail_jual.kd_jual, detail_jual.total_harga, detail_jual.kd_barang FROM trans_jual  LEFT JOIN detail_jual on detail_jual.kd_jual = trans_jual.kd_jual LEFT JOIN tb_pelanggan on tb_pelanggan.kd_pelanggan = trans_jual.kd_pelanggan WHERE tgl>='$tgl_awal' AND tgl <='$tgl_akhir'";
+// 		$total = "SELECT sum(total_harga) as total_harga, sum(jumlah) as jumlah, sum(diskon) as diskon FROM detail_jual INNER JOIN trans_jual ON trans_jual.kd_jual = detail_jual.kd_jual WHERE trans_jual.tgl>='$tgl_awal' AND trans_jual.tgl <='$tgl_akhir' ORDER BY tgl asc";
+// 	}
+// }
 
 
 $pdf = new FPDF("L","cm","A4");
@@ -47,43 +49,32 @@ $pdf->SetFont('Arial','B',10);
 $pdf->Cell(5,0.7,"Di cetak pada : ".date("D-d/m/Y"),0,0,'C');
 $pdf->ln(1);
 $pdf->SetFont('Arial','B',10);
-$pdf->Cell(1, 0.8, 'No', 1, 0, 'C');
-$pdf->Cell(2, 0.8, 'Tanggal', 1, 0, 'C');
-$pdf->Cell(4, 0.8, 'No Transaksi', 1, 0, 'C');
-$pdf->Cell(3, 0.8, 'Nama Pelanggan', 1, 0, 'C');
-$pdf->Cell(3, 0.8, 'Nama Barang', 1, 0, 'C');
-$pdf->Cell(2, 0.8, 'Jumlah', 1, 0, 'C');
-$pdf->Cell(3, 0.8, 'Harga', 1, 0, 'C');
-$pdf->Cell(3, 0.8, 'Diskon', 1, 0, 'C');
-$pdf->Cell(3, 0.8, 'Harga Total', 1, 1, 'C');
+$pdf->Cell(1, 0.8, 'NO', 1, 0, 'C');
+$pdf->Cell(2, 0.8, 'ID Pinjam', 1, 0, 'C');
+$pdf->Cell(4, 0.8, 'Kode Buku Literasi', 1, 0, 'C');
+$pdf->Cell(3, 0.8, 'NIS Peminjam', 1, 0, 'C');
+$pdf->Cell(3, 0.8, 'Jatuh Tempo', 1, 0, 'C');
+$pdf->Cell(3, 0.8, 'Tanggal Kembali', 1, 0, 'C');
+$pdf->Cell(2, 0.8, 'Terlambat', 1, 0, 'C');
+$pdf->Cell(3, 0.8, 'Denda', 1, 0, 'C');
+$pdf->Cell(3, 0.8, 'Notifikasi', 1, 1, 'C');
 $pdf->SetFont('Arial','',10);
 $no=1;
-$query=mysqli_query($conn, $query1);
-$totale = mysqli_query($conn, $total);
-$hasil = mysqli_fetch_array($totale);
-while($lihat=mysqli_fetch_array($query)){
-	$kd_barang = $lihat['kd_barang'];
-	$sala = "SELECT * FROM tb_barang WHERE kd_barang='$kd_barang'";
-	$sele = mysqli_query($conn, $sala);
-	$hasilnya = mysqli_fetch_array($sele);
+
+foreach( $peminjaman_literasi as $lihat) :{
 	$pdf->Cell(1, 0.8, $no , 1, 0, 'C');
-	$pdf->Cell(2, 0.8, $lihat['tgl'] , 1, 0, 'C');
-	$pdf->Cell(4, 0.8, $lihat['kd_jual'],1, 0, 'C');
-	$pdf->Cell(3, 0.8, $lihat['nama_pelanggan'], 1, 0,'C');
-	$pdf->Cell(3, 0.8, $hasilnya['nama_barang'],1, 0, 'C');
-	$pdf->Cell(2, 0.8, $lihat['jumlah'], 1, 0,'R');
-	$pdf->Cell(3, 0.8, "Rp. ".number_format($lihat['harga_normal']).",-", 1, 0, 'R');
-	$pdf->Cell(3, 0.8, "Rp. ".number_format($lihat['diskon']).",-", 1, 0, 'R');
-	$pdf->Cell(3, 0.8, "Rp. ".number_format($lihat['total_harga']).",-", 1, 1,'R');
-
+	$pdf->Cell(2, 0.8, $lihat['id_pinjam_buku_literasi'] , 1, 0, 'C');
+	$pdf->Cell(4, 0.8, $lihat['kode_buku_literasi'],1, 0, 'C');
+	$pdf->Cell(3, 0.8, $lihat['nis'], 1, 0,'C');
+	$pdf->Cell(3, 0.8, $lihat['tanggal_hrs_kembali'],1, 0, 'C');
+	$pdf->Cell(3, 0.8, $lihat['tanggal_pengembalian'], 1, 0,'R');
+	$pdf->Cell(2, 0.8, $lihat['terlambat'], 1, 0,'C');
+	$pdf->Cell(3, 0.8, $lihat['denda'], 1, 0,'C');
+	$pdf->Cell(3, 0.8, $lihat['notifikasi'], 1, 1,'C');
 	$no++;
+	
 }
-$pdf->SetFont('Arial','B',10);
-$pdf->Cell(13, 0.8, "TOTAL", 1, 0,'R');
-$pdf->Cell(2, 0.8, $hasil['jumlah'], 1, 0,'R');
-$pdf->Cell(6, 0.8, "Rp. ".number_format($hasil['diskon']).",-",1, 0,'R');
-$pdf->Cell(3, 0.8, "Rp. ".number_format($hasil['total_harga']).",-",1, 1,'R');
-
+endforeach; 
 # footer
 $pdf->Ln();$pdf->Ln();
 $pdf->SetFont('Arial','',9);
