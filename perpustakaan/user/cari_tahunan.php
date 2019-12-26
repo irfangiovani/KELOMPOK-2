c84c0714428e13a2a9ccc55af79f7668ce02d696
azizah wina <azizahwinasriwinarsih24@gmail.com>
2019-12-26 15:05:57 +0700

    cari tahunan

--- perpustakaan/user/cari_tahunan.php
+++ perpustakaan/user/cari_tahunan.php
@@ -7,6 +7,12 @@ require 'functions.php';
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+<section class="contact">
+    <title> Peminjaman </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</section>
+
   <meta charset="utf-8">
   <title>K-Negabon Library</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
@@ -43,28 +49,6 @@ require 'functions.php';
 <body>
 <div class="click-closed"></div>
   <!--/ Form Search Star /-->
-  <div class="box-collapse">
-    <div class="title-box-d">  
-      <h3 class="title-d">Cari Peminjaman Siswa</h3>
-    </div>
-    <span class="close-box-collapse right-boxed ion-ios-close"></span>
-    <div class="box-collapse-wrap form">
-      <form class="form-a" action ="data_cari_tahunan.php" method="post">
-        <div class="row">
-          <div class="col-md-12 mb-2">
-            <div class="form-group">
-              <label for="Type">Nomor Induk Siswa</label>
-              <input type="text" class="form-control form-control-lg form-control-a" name="nis" placeholder="Masukkkan NIS anda">
-            </div>
-          </div>
-          <div class="col-md-12">
-
-          <button <a href ="cari_tahunan.php" type="submit" name = "cari_tahunan" class="btn btn-b"></a>cari tahunan</button>
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
  
   <!--/ Form Search End /-->
 
@@ -95,13 +79,46 @@ require 'functions.php';
           </li>
         </ul>
       </div>
-      <button type="button" class="btn btn-b-n navbar-toggle-box-collapse d-none d-md-block  " data-toggle="collapse"
-        data-target="#navbarTogglerDemo01" aria-expanded="false">
-        <span class="search" aria-hidden="true">Cek Peminjaman</span>
-      </button>
     </div>
   </nav>
   <!--/ Nav End /-->
+                      
+  <section class="contact">
+              <style>
+                body {
+                  background: url(img/sunset.jpg) no-repeat center fixed;
+                  -webkit-backgroun-size: cover;
+                  -moz-background-size: cover;
+                  -o-background: cover;
+                  background-size: cover;
+                }
+                .aa {
+                  width: 700px;
+                  height: 400px;
+                  background-color: rgba(0,0,0,0.3);
+                  margin: 0 auto;
+                  margin-top: 150px;
+                  padding-top: -8px;
+                  padding-left: 80px;
+                  border-radius: 60px;
+                  -webkit-border-radius: 90px;
+                  -o-border-radius: 90px;
+                  -moz-border-radius: 90px;
+                  color: darkred;
+                }
+              </style>
+  </section>
+   
+  <section class="contact">
+   <div class="aa">
+     <br> <br> <br> <br> <br>
+     <h2>Cek Peminjaman</h2><br>
+     <form action ="data_cari_tahunan.php" method="post">
+        <input type="text" name="nis"placeholder="Nomor Induk Siswa"><br><br>
+        <button <a href ="cari_tahunan.php" type="submit" name = "cari_tahunan" class="btn btn-b"></a>cari tahunan</button>
+    </form>   
+  </section>
+   
 
    <!-- JavaScript Libraries -->
    <script src="lib/jquery/jquery.min.js"></script>
@@ -119,3 +136,6 @@ require 'functions.php';
 
   </body>
 </html>
+
+
+
